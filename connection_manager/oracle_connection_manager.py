ba7ddf4d612d0e66731c4ba832747d2e7f4ea49b
Atul <aatulgoel@gmail.com>
2018-10-17 05:36:33 +0100

    Changed Oracle_connection to use sqlalchemy
    Able to commit to DB now

--- connection_manager/oracle_connection_manager.py
+++ connection_manager/oracle_connection_manager.py
@@ -1,32 +1,47 @@
 class ManageConnection:
 
-    def __init__(self):
-        oracle_connection = None
-
-    def create_connection(self):
+    def get_connection(self):
         import cx_Oracle
+        from sqlalchemy import create_engine
+
+        host = 'localhost'
+        port = '1521'
+        sid = 'xe'
+        user = 'url_matcher'
+        password = 'url_matcher'
+        sid = cx_Oracle.makedsn(host, port, sid=sid)
+
+        connection_string = 'oracle://{user}:{password}@{sid}'.format(
+            user=user,
+            password=password,
+            sid=sid
+        )
+
         try:
-            connection_string = "url_matcher/url_matcher@localhost"
-            self.oracle_connection = cx_Oracle.connect(connection_string)
-            conn = self.oracle_connection
-            return conn
+            engine = create_engine(
+                connection_string,
+                convert_unicode=False,
+                pool_recycle=10,
+                pool_size=50,
+                echo=True
+            )
+
+            connection = engine.connect()
+            return connection
+
         except Exception as e:
             print(e)
 
     def test_connection(self):
-        cur = self.oracle_connection.cursor()
-        cur.execute("select * from dual")
 
-        for dummy in cur.fetchall():
-            print(dummy)
-        cur.close()
+        connection = self.get_connection()
 
-    def distroy_connection(self):
-        self.oracle_connection.close()
+        result = connection.execute('select * from DUAL')
+        for row in result:
+            print(row)
 
 
 if __name__ == '__main__':
     test_oracle_connection = ManageConnection()
-    test_oracle_connection.create_connection()
+    test_oracle_connection.get_connection()
     test_oracle_connection.test_connection()
-    test_oracle_connection.distroy_connection()
--- url_matcher/match_url.py
+++ url_matcher/match_url.py
@@ -2,48 +2,46 @@ import connection_manager.oracle_connection_manager as cm
 import traceback
 import pandas as pd
 
-
 def load_data_from_db(sql_stmt):
     try:
-        conn_handler = cm.ManageConnection()
-        conn = conn_handler.create_connection()
+        connection = cm.ManageConnection().get_connection()
 
-        df = pd.read_sql(sql_stmt, con=conn)
+        df = pd.read_sql(sql_stmt, con=connection)
         return df
 
     except Exception as e:
         traceback.print_exc(e)
 
-    finally:
-        conn_handler.distroy_connection()
 
 def url_matcher():
-    conn_handler = cm.ManageConnection()
-    conn = conn_handler.create_connection()
+    connection = cm.ManageConnection().get_connection()
     try:
         raw_data_df = load_data_from_db("select * from raw_data")
         matched_data_df = load_data_from_db("select * from matched_data")
-
         data_to_match_df = pd.read_csv("../log_of_urls_invoked.csv")
-
-        print(data_to_match_df.head(5))
+        data_to_match_df["tokens"] = data_to_match_df["URL"].str.strip("'").str.strip('/').str.split('/')
+        data_to_match_df["token_count"] = data_to_match_df["tokens"].str.len()
 
         for row in data_to_match_df.itertuples():
             if len(matched_data_df) == 0 and len(raw_data_df) == 0:
-                data_dict = {"id": 1, "raw_url": row.URL, "hit_count": "1", \
-                              "service_providing_system":row.sourceIP, "service_using_system": row.appName,"token_count":"1",\
-                              "matched_data_id":"1"}
-                print(data_dict)
-                # data_dict_df = pd.DataFrame.from_dict(data_dict, orient='index')
-                data_dict_df = pd.DataFrame(data_dict, index=[0])
 
-                raw_data_df.append(data_dict_df, ignore_index=False)
+                raw_data_id_result = connection.execute("select raw_data_seq.nextval as seqval from dual")
 
-                raw_data_df.to_sql('RAW_DATA', conn, if_exists='replace')
+                for seq_result in raw_data_id_result:
+                    raw_data_id = seq_result.seqval
+
+                token_string = ', '.join(row.tokens)
+
+                data_dict = {"id": raw_data_id, "raw_url": row.URL, "hit_count": 1, \
+                              "service_providing_system":row.sourceIP, "service_using_system": row.appName,\
+                             "token_count":row.token_count, "tokens":token_string}
+                data_dict_df = pd.DataFrame(data_dict, index=[id])
+
+                raw_data_df.append(data_dict_df, sort=True)
+
+                data_dict_df.to_sql('RAW_DATA', connection, if_exists='append', index=False)
 
                 print(data_dict_df.head())
-
-
     except Exception as e:
         traceback.print_exc(e)
 
