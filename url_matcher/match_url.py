bcfffe9dcaa838708359e85513ecf2fcad28f87c
Atul <aatulgoel@gmail.com>
2018-10-17 09:05:14 +0100

    Refactored to get primary key id and added hamming score

--- url_matcher/match_url.py
+++ url_matcher/match_url.py
@@ -12,6 +12,22 @@ def load_data_from_db(sql_stmt):
     except Exception as e:
         traceback.print_exc(e)
 
+def get_primary_key_id():
+    connection = cm.ManageConnection().get_connection()
+    raw_data_id_result = connection.execute("select raw_data_seq.nextval as seqval from dual")
+
+    for seq_result in raw_data_id_result:
+        raw_data_id = seq_result.seqval
+
+    return raw_data_id
+
+def get_hamming_score(list1,list2):
+    hamming_score = 0
+    for index in range(len(list1)):
+        if list1[index] != list2[index]:
+            hamming_score += 1
+    return hamming_score
+
 
 def url_matcher():
     connection = cm.ManageConnection().get_connection()
@@ -24,24 +40,33 @@ def url_matcher():
 
         for row in data_to_match_df.itertuples():
             if len(matched_data_df) == 0 and len(raw_data_df) == 0:
+                token_string = ','.join(row.tokens)
+                type(token_string)
 
-                raw_data_id_result = connection.execute("select raw_data_seq.nextval as seqval from dual")
-
-                for seq_result in raw_data_id_result:
-                    raw_data_id = seq_result.seqval
-
-                token_string = ', '.join(row.tokens)
-
-                data_dict = {"id": raw_data_id, "raw_url": row.URL, "hit_count": 1, \
+                data_dict = {"id": get_primary_key_id(), "raw_url": row.URL, "hit_count": 1, \
                               "service_providing_system":row.sourceIP, "service_using_system": row.appName,\
                              "token_count":row.token_count, "tokens":token_string}
                 data_dict_df = pd.DataFrame(data_dict, index=[id])
 
-                raw_data_df.append(data_dict_df, sort=True)
-
                 data_dict_df.to_sql('RAW_DATA', connection, if_exists='append', index=False)
 
-                print(data_dict_df.head())
+                raw_data_df = raw_data_df.append(data_dict_df, sort=True)
+
+            elif len(matched_data_df) == 0:
+                same_token_count_df = raw_data_df[(raw_data_df.token_count == row.token_count)]
+                #print(row.tokens , "row of tokens")
+                #print(raw_data_df.head())
+                #print(raw_data_df["tokens"].str.split() , "token string")
+                
+                for same_token_count_df_row in same_token_count_df.itertuples():
+                    hamming_score = get_hamming_score(same_token_count_df_row.tokens.split(','), row.tokens)
+
+                print(hamming_score)
+
+
+
+
+
     except Exception as e:
         traceback.print_exc(e)
 
