974d4154f171d160018c098da402ef76fe6eaa7b
Atul <aatulgoel@gmail.com>
2018-10-18 06:26:08 +0100

    refactored

--- url_matcher/match_url.py
+++ url_matcher/match_url.py
@@ -15,6 +15,7 @@ def load_data_from_db(sql_stmt):
 def get_primary_key():
     connection = cm.ManageConnection().get_connection()
     raw_data_id_result = connection.execute("select raw_data_seq.nextval as seqval from dual")
+    raw_data_id = None
     for seq_result in raw_data_id_result:
         raw_data_id = seq_result.seqval
     return raw_data_id
@@ -41,9 +42,7 @@ def get_potential_matched_url(tokens, mismatch_postion):
 
 
 def url_matcher():
-    connection = cm.ManageConnection().get_connection()
     try:
-
         # Initialize dataframes and add required columns
         raw_data_df = load_data_from_db("select * from raw_data")
         matched_data_df = load_data_from_db("select * from matched_data")
@@ -58,88 +57,98 @@ def url_matcher():
             # If both matched_data_df and raw_data_df are blank means the System is new
             # So we just insert the first fow of csv_log_file_df into raw_data_df
             if len(matched_data_df) == 0 and len(raw_data_df) == 0:
-
                 # Create a dictionary
                 # Convert dictionary to a dataframe
                 # Append one dataFrame to another
                 token_string = ','.join(row.tokens)
-                raw_data_dict = {"id": get_primary_key(),
-                                 "raw_url": row.URL,
-                                 "hit_count": 1,
-                                 "service_providing_system": row.sourceIP,
-                                 "service_using_system": row.appName,
-                                 "token_count": row.token_count,
-                                 "tokens": token_string,
-                                 "modified_flag": True}
+                raw_data_dict = get_raw_data_dict(row, token_string)
                 data_dict_df = pd.DataFrame(raw_data_dict, index=[id])
                 raw_data_df = raw_data_df.append(data_dict_df, sort=True)
 
             # If matched_data_df is blank but raw_data_df has data
             # We can find a potential match
             elif len(matched_data_df) == 0:
-                # Filter raw_data_df using token_count as criteria
-                # Should be a big performance boost in long run
-                df_filtered_on_token_count = raw_data_df[(raw_data_df.token_count == row.token_count)]
-
-                # Loop through df_filtered_on_token_count to find a potential match
-                # Get Hamming Score
-                for raw_data_row in df_filtered_on_token_count.itertuples():
-                    hamming_score, mismatch_token_index_str = get_hamming_score(raw_data_row.tokens.split(','),
-                                                                                row.tokens)
-
-                    # If hamming Score = 0 then Current row matches exactly with a row in raw_data_df
-                    # In this case we just increase the hit count
-                    if hamming_score == 0:
-                        # Get raw_data_df row to update
-                        # Update hit_count and mark modified_flag = True
-                        # So we can filter on flag to upsert
-                        raw_data_df_row = raw_data_df.loc[raw_data_df["id"] == raw_data_row.id]
-                        raw_data_df.loc[raw_data_df["id"] == raw_data_row.id, ["hit_count", "modified_flag"]] = \
-                            raw_data_df_row['hit_count'].iloc[0] + 1, True
-
-                    # If hamming Score = 1 then
-                    # We Found a potential match
-                    # We add row into matched_data_df
-                    # We update raw_data_df foreign key
-                    elif hamming_score == 1:
-                        token_string = ','.join(row.tokens)
-                        potential_matched_url = get_potential_matched_url(row.tokens, mismatch_token_index_str)
-                        matched_data_dict = {"id": get_primary_key(),
-                                             "potential_matched_url": potential_matched_url,
-                                             "hamming_score": hamming_score,
-                                             "tokens": token_string,
-                                             "token_position": mismatch_token_index_str,
-                                             "token_count": row.token_count,
-                                             "service_providing_system": row.sourceIP,
-                                             "final_matched_url": None,
-                                             "auto_matched": None,
-                                             "auto_matched_verified": None,
-                                             "false_positive": None,
-                                             "housekeep_raw_data": None,
-                                             "modified_flag": True}
-                        matched_data_dict_to_df = pd.DataFrame(matched_data_dict, index=["id"])
-                        matched_data_df = matched_data_df.append(matched_data_dict_to_df, sort=True)
-                    else:
-                        # If hamming_score > 1
-                        # Not handling such cases now
-                        # But we may want to support in future
-                        # So a TODO
-                        # For now in such case, it is just another raw_data with no match
-                        # so just insert in raw_data_df
-                        raw_data_dict = {"id": get_primary_key(),
-                                         "raw_url": row.URL,
-                                         "hit_count": 1,
-                                         "service_providing_system": row.sourceIP,
-                                         "service_using_system": row.appName,
-                                         "token_count": row.token_count,
-                                         "tokens": token_string,
-                                         "modified_flag": True}
-                        data_dict_df = pd.DataFrame(raw_data_dict, index=[id])
-                        raw_data_df = raw_data_df.append(data_dict_df, sort=True)
+                matched_data_df, raw_data_df = check_raw_data_match(matched_data_df, raw_data_df, row)
             else:
                 pass
     except Exception as e:
         traceback.print_exc(e)
+
+
+def check_raw_data_match(matched_data_df, raw_data_df, row):
+    # Filter raw_data_df using token_count as criteria
+    # Should be a big performance boost in long run
+    df_filtered_on_token_count = raw_data_df[(raw_data_df.token_count == row.token_count)]
+    # Loop through df_filtered_on_token_count to find a potential match
+    # Get Hamming Score
+    df_filtered_on_token_count_size = df_filtered_on_token_count.shape[0]
+    for index, raw_data_row in enumerate(df_filtered_on_token_count.itertuples(), 1):
+        hamming_score, mismatch_token_index_str = get_hamming_score(raw_data_row.tokens.split(','),
+                                                                    row.tokens)
+        token_string = ','.join(row.tokens)
+        # If hamming Score = 0 then Current row matches exactly with a row in raw_data_df
+        # In this case we just increase the hit count
+        if hamming_score == 0:
+            # Get raw_data_df row to update
+            # Update hit_count and mark modified_flag = True
+            # So we can filter on flag to upsert
+            raw_data_df_row = raw_data_df.loc[raw_data_df["id"] == raw_data_row.id]
+            raw_data_df.loc[raw_data_df["id"] == raw_data_row.id, ["hit_count", "modified_flag"]] = \
+                raw_data_df_row['hit_count'].iloc[0] + 1, True
+            break
+        # If hamming Score = 1 then
+        # We Found a potential match
+        # We add row into matched_data_df
+        # We update raw_data_df foreign key
+        elif hamming_score == 1:
+            potential_matched_url = get_potential_matched_url(row.tokens, mismatch_token_index_str)
+            matched_data_dict = get_matched_data_dict(hamming_score, mismatch_token_index_str, potential_matched_url,
+                                                      row, token_string)
+            matched_data_dict_to_df = pd.DataFrame(matched_data_dict, index=["id"])
+            matched_data_df = matched_data_df.append(matched_data_dict_to_df, sort=True)
+            break
+        else:
+            # If hamming_score > 1
+            # Not handling such cases now
+            # But we may want to support in future
+            # So a TODO
+            # For now in such case, it is just another raw_data with no match
+            # so just insert in raw_data_df
+            # When we are in the last iteration
+            if df_filtered_on_token_count_size == index:
+                raw_data_dict = get_raw_data_dict(row, token_string)
+                data_dict_df = pd.DataFrame(raw_data_dict, index=[id])
+                raw_data_df = raw_data_df.append(data_dict_df, sort=True)
+    return matched_data_df, raw_data_df
+
+
+def get_matched_data_dict(hamming_score, mismatch_token_index_str, potential_matched_url, row, token_string):
+    matched_data_dict = {"id": get_primary_key(),
+                         "potential_matched_url": potential_matched_url,
+                         "hamming_score": hamming_score,
+                         "tokens": token_string,
+                         "token_position": mismatch_token_index_str,
+                         "token_count": row.token_count,
+                         "service_providing_system": row.sourceIP,
+                         "final_matched_url": None,
+                         "auto_matched": None,
+                         "auto_matched_verified": None,
+                         "false_positive": None,
+                         "housekeep_raw_data": None,
+                         "modified_flag": True}
+    return matched_data_dict
+
+
+def get_raw_data_dict(row, token_string):
+    raw_data_dict = {"id": get_primary_key(),
+                     "raw_url": row.URL,
+                     "hit_count": 1,
+                     "service_providing_system": row.sourceIP,
+                     "service_using_system": row.appName,
+                     "token_count": row.token_count,
+                     "tokens": token_string,
+                     "modified_flag": True}
+    return raw_data_dict
 
 
 url_matcher()
